b1c48b9c223fbb06771bee87168ce8422d0b701d
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-06-13 16:51:12 +0200

    windows templates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,34 @@
 'use strict';
 
 angular.module('newsGameApp')
-	.controller('MainCtrl', function($scope) {
+	.controller('MainCtrl', function($scope, $log, $timeout) {
 
+		$scope.templates = [
+			'lorem'
+		];
 
-		$scope.hello = "Hello from Controller!";
+		$scope.windows = {
+			win1: {
+				title: 'windows 1',
+				visible: false,
+				template: 'lorem'
+			},
+			win2: {
+				title: 'windows 2',
+				visible: false,
+				template: 'lorem'
+			}
+		};
+
+		$scope.openWin = function(id) {
+			$log.log(id);
+			jQuery('#' + id).data('kendoWindow').open();
+		};
+
+		$timeout(function() {
+
+			$log.log('ready');
+
+		}, 50);
 
 	});
