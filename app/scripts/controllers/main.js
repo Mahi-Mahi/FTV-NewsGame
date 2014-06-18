964c933161e8076eaef7d087be108f9fc11e37c4
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-06-18 18:28:20 +0200

    fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,14 +3,53 @@
 angular.module('newsGameApp')
 	.controller('MainCtrl', function($scope, $log, $timeout) {
 
-		$scope.templates = [
-			'lorem'
-		];
-
 		$scope.windows = {};
+
+		// Cuitcuitter
+
+		$scope.cuits = {
+			'cuit-1': {
+				author: '@toto',
+				content: 'Cras quis eleifend arcu, in. Ut aliquam sapien sed felis.'
+			},
+			'cuit-2': {
+				author: '@foo',
+				content: 'Aenean mattis, lacus at eleifend. Etiam rhoncus iaculis ante eu.'
+			}
+		};
+
 		$scope.currentCuit = null;
 
-		$scope.truc = 'machin';
+		$scope.openCuit = function(id) {
+			$log.log("openCuit(" + id);
+			$scope.closeWin('cuit');
+			$scope.currentCuit = $scope.cuits[id];
+			$scope.openWin('cuit');
+		};
+
+		// Skoupe
+
+		$scope.contacts = {
+			'jean-luc': {
+				name: 'Jean Luc',
+				content: 'Cras quis eleifend arcu, in. Ut aliquam sapien sed felis.'
+			},
+			'jean-michel': {
+				name: 'Jean-Michel',
+				content: 'Aenean mattis, lacus at eleifend. Etiam rhoncus iaculis ante eu.'
+			}
+		};
+
+		$scope.currentContact = null;
+
+		$scope.openContact = function(id) {
+			$log.log("openContact(" + id);
+			$scope.closeWin('contact');
+			$scope.currentContact = $scope.contacts[id];
+			$scope.openWin('contact');
+		};
+
+		// Generic Window Management
 
 		function createWindow(id, args) {
 			$scope.windows[id] = angular.extend({
@@ -26,31 +65,11 @@ angular.module('newsGameApp')
 			}, args);
 		}
 
-		function loadCuits() {
-			$log.log('loadCuits');
-			$scope.cuits = {
-				'cuit-1': {
-					author: '@toto',
-					content: 'Cras quis eleifend arcu, in. Ut aliquam sapien sed felis.'
-				},
-				'cuit-2': {
-					author: '@foo',
-					content: 'Aenean mattis, lacus at eleifend. Etiam rhoncus iaculis ante eu.'
-				}
-			};
-		}
-
-		$scope.openCuit = function(id) {
-			$log.log("openCuit(" + id);
-			$scope.currentCuit = $scope.cuits[id];
-			$scope.openWin('cuit');
-		};
-
 		$scope.toggleWin = function(id) {
 			if ($scope.windows[id].visible) {
-				jQuery('#' + id).data('kendoWindow').close();
+				$scope.closeWin(id);
 			} else {
-				jQuery('#' + id).data('kendoWindow').open();
+				$scope.openWin(id);
 			}
 		};
 
@@ -59,12 +78,22 @@ angular.module('newsGameApp')
 				jQuery('#' + id).data('kendoWindow').open();
 			}
 		};
+		$scope.closeWin = function(id) {
+			if ($scope.windows[id].visible) {
+				jQuery('#' + id).data('kendoWindow').close();
+				switch (id) {
+					case 'cuicuiter':
+						$log.log("cuit");
+						$scope.closeWin('cuit');
+						break;
+					case 'skoupe':
+						$scope.closeWin('contact');
+						break;
+				}
+			}
+		};
 
-		function activateWin(id) {
-
-		}
-
-		loadCuits();
+		// Initialisation
 
 		createWindow('cuicuiter', {
 			title: 'Cuicuiter',
@@ -92,6 +121,15 @@ angular.module('newsGameApp')
 			position: {
 				top: 150,
 				left: 600
+			}
+		});
+		createWindow('contact', {
+			title: 'contact',
+			active: false,
+			template: 'skoupe-contact',
+			position: {
+				top: 125,
+				left: 550
 			}
 		});
 
